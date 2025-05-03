086b07918786d7a5bd6441ed9c8e6a28beb2c1db
borahacikoylu <borahckyl1@gmail.com>
2025-05-03 14:56:57 +0300

    Homescreen big update

--- frontend/context/index.js
+++ frontend/context/index.js
@@ -1,12 +1,18 @@
 import { createContext } from "react";
+import { useState } from "react";
+import React, { useContext } from 'react';
 
 
 export const GlobalContext = createContext(null);
 
 
 function GlobalState({ children }) {
+    const [showLoginView, setshowLoginView] = useState(false);
+    const [currentUserName, setCurrentUserName] = useState('');
+    const [currentUser, setCurrentUser] = useState("");
+    const [allUsers, setAllUsers] = useState([]);
     return (
-        <GlobalContext.Provider value={{}}>
+        <GlobalContext.Provider value={{ showLoginView, setshowLoginView, currentUserName, setCurrentUserName, currentUser, setCurrentUser, allUsers, setAllUsers }}>
             {children}
         </GlobalContext.Provider>
     );
